6d39ff96f0645051271b5c0efbf5157d5eb6e74e
aoaochan <josejbeck99@yahoo.com>
2025-06-27 11:04:33 +0900

    ~double

--- asdf_qwer.c
+++ asdf_qwer.c
@@ -9,9 +9,23 @@ int main(void) { // int main(int argc, char **argv) {
     // printf("It's really good!\n");
     // return 0;
 
-    int age = 5521;
-    int year = 2025;
-    int quantity = 2; // 1;
-    printf("age: %d\nyear: %d\nquantity: %d\n", age, year, quantity);
+    // int age = 5521;
+    // int year = 2025;
+    // int quantity = 2; // 1;
+    // printf("age: %d\nyear: %d\nquantity: %d\n", age, year, quantity);
+    // return 0;
+
+    // float gpa = 2.5;
+    // float price = 19.99;
+    // float temp = -10.1;
+    // printf("gpa: %.1f\n", gpa);
+    // printf("price: $%.2f\n", price);
+    // printf("temp: %f", temp);
+    // return 0;
+
+    double pi = 3.14159265359;
+    double e = 2.7182818284590;
+    printf("pi: %.15lf\n", pi); // lf = long float
+    printf("e: %.15lf\n", e);
     return 0;
 }
