2925c6b3677d5b159a71965a56b8acb1250dd43f
aoaochan <josejbeck99@yahoo.com>
2025-06-27 10:53:27 +0900

    meme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # c-study
+
+## Credits
+- https://mesonbuild.com/
+- [C Programming Full Course for free ⚙️ (2025)](https://youtu.be/xND0t1pr3KY?si=kWgyrKMrlaE66EgT)
--- asdf_qwer.c
+++ asdf_qwer.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-    if (argc != 1) printf("(argc: %d, argv: %s)\n", argc, argv[0]);
-    printf("Hello, world!\n");
+int main(void) { // int main(int argc, char **argv) {
+    // if (argc != 1) printf("(argc: %d, argv: %s)\n", argc, argv[0]);
+    // printf("Hello, world!\n");
+    // return 0;
+
+    // printf("I like pizza!\n");
+    // printf("It's really good!\n");
+    // return 0;
+
+    int age = 5521;
+    int year = 2025;
+    int quantity = 2; // 1;
+    printf("age: %d\nyear: %d\nquantity: %d\n", age, year, quantity);
     return 0;
 }
